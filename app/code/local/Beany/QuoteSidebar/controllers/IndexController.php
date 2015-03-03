bd3b3c8853c37c9041c6e8ebc7f4b3f5fd3ebb67
dungng <vuquanghoang@hotmail1.com>
2015-03-03 15:51:56 +0700

    update

--- app/code/local/Beany/QuoteSidebar/controllers/IndexController.php
+++ app/code/local/Beany/QuoteSidebar/controllers/IndexController.php
@@ -1,8 +1,9 @@
 <?php
 class Beany_QuoteSidebar_IndexController extends Mage_Core_Controller_Front_Action {
 	public function indexAction() {
-		echo 123; die(123);
 		$params = $this->getRequest()->getParams();
-		mail('vuquanghoang@hotmail.com', 'quick quote from usb2c', $params['name'] . ' quoted ' . $params['quantity'] . ' ' . ' of capacity ' . $params['capacity']);
-	} 
+		mail('vuquanghoang@hotmail.com', 'quick quote from usb2c', $params['name'] .
+            ' quoted ' . $params['quantity'] . ' ' . ' of capacity ' .
+            $params['capacity'] . "\n" . $params['content']);
+	}
 }
